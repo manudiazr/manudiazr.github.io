3cbe8a0f515a5c4458e84bedad77dbc0123b628c
Manuel Diaz <manueldiaz.travel@gmail.com>
2018-06-02 13:58:45 -0500

    inicializacion de proyecto, formulario funcionando y plantilla de marcador realizada

---
+++ send_form_email.php
@@ -0,0 +1,22 @@
+<?php
+$nombre = $_POST['nombre'];
+$equipo_1 = $_POST['equipo_1'];
+$equipo_2 = $_POST['equipo_2'];
+
+$header = 'From: ' . $nombre . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Nombre= " . $nombre . ",
+Equipo 1 = " . $equipo_1 . " \r\n";
+$mensaje .= "Equipo 2= " . $equipo_2 . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'manuddr.1993@gmail.com';
+$asunto = 'marcador mundial';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+echo 'Mensaje enviado correctamente';
+?>
